838dfe012c0ce9543947476ba65b8a9939f2c37d
Aristeu-Dantas <aristeu_dantas@hotmail.com>
2023-07-19 11:08:57 -0300

    Update structural

--- Avaliacao-Dados-Servidores/Dados-Servidores.sql
+++ Avaliacao-Dados-Servidores/Dados-Servidores.sql
@@ -1,63 +1,65 @@
-CREATE TABLE disciplina_ingresso (
-    id_disciplina BIGINT NOT NULL,
-    nome_disciplina VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_disciplina)
+CREATE TABLE "servidor"(
+    "matricula" BIGINT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "categoria" VARCHAR(255) NOT NULL,
+    "cargo" BIGINT NOT NULL,
+    "curriculo_lattes" VARCHAR(255) NOT NULL,
+    "urk_foto_75x100" VARCHAR(255) NOT NULL,
+    "id_setor" SERIAL NOT NULL,
+    "id_disciplina" SERIAL NOT NULL,
+    "id_funcao" SERIAL NOT NULL,
+    "id_jornada" SERIAL NOT NULL,
+    "id_campus" SERIAL NOT NULL,
+    "id_telefones" SERIAL NOT NULL
 );
-
-CREATE TABLE telefone_institucionais (
-    id_telefone BIGINT NOT NULL,
-    ramal BIGINT NOT NULL,
-    numero_telefone BIGINT NOT NULL,
-    PRIMARY KEY (id_telefone)
+ALTER TABLE
+    "servidor" ADD PRIMARY KEY("matricula");
+CREATE TABLE "jornada_trabalho"(
+    "id_jornada" SERIAL NOT NULL,
+    "JornadaTrabalho" VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE funcao (
-    id_funcao BIGINT NOT NULL,
-    nome_funcao VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_funcao)
+ALTER TABLE
+    "jornada_trabalho" ADD PRIMARY KEY("id_jornada");
+CREATE TABLE "setor_suap"(
+    "id_setor" SERIAL NOT NULL,
+    "nome_setor" VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE setor_suap (
-    id_setor BIGINT NOT NULL,
-    nome_setor VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_setor)
+ALTER TABLE
+    "setor_suap" ADD PRIMARY KEY("id_setor");
+CREATE TABLE "campus"(
+    "id_campus" SERIAL NOT NULL,
+    "nome_campus" VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE campus (
-    id_campus BIGINT NOT NULL,
-    nome_campus VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_campus)
+ALTER TABLE
+    "campus" ADD PRIMARY KEY("id_campus");
+CREATE TABLE "telefone_institucionais"(
+    "id_telefone" SERIAL NOT NULL,
+    "ramal" BIGINT NOT NULL,
+    "numero_telefone" BIGINT NOT NULL
 );
-
-CREATE TABLE jornada_trabalho (
-    id_jornada BIGINT NOT NULL,
-    JornadaTrabalho VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_jornada)
+ALTER TABLE
+    "telefone_institucionais" ADD PRIMARY KEY("id_telefone");
+CREATE TABLE "funcao"(
+    "id_funcao" SERIAL NOT NULL,
+    "nome_funcao" BIGINT NOT NULL
 );
-
-CREATE TABLE servidor (
-    matricula BIGINT NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    categoria VARCHAR(255) NOT NULL,
-    cargo VARCHAR(255) NOT NULL,
-    curriculo_lattes VARCHAR(255) NOT NULL,
-    url_foto_75x100 VARCHAR(255) NOT NULL,
-    id_setor BIGINT NOT NULL,
-    id_disciplina BIGINT NOT NULL,
-    id_funcao BIGINT NOT NULL,
-    id_jornada BIGINT NOT NULL,
-    id_campus BIGINT NOT NULL,
-    id_telefones BIGINT NOT NULL,
-    PRIMARY KEY (matricula),
-    FOREIGN KEY (id_funcao) REFERENCES funcao (id_funcao),
-    FOREIGN KEY (id_campus) REFERENCES campus (id_campus),
-    FOREIGN KEY (id_jornada) REFERENCES jornada_trabalho (id_jornada),
-    FOREIGN KEY (id_disciplina) REFERENCES disciplina_ingresso (id_disciplina),
-    FOREIGN KEY (id_telefones) REFERENCES telefone_institucionais (id_telefone),
-    FOREIGN KEY (id_setor) REFERENCES setor_suap (id_setor)
+ALTER TABLE
+    "funcao" ADD PRIMARY KEY("id_funcao");
+CREATE TABLE "disciplina_ingresso"(
+    "id_disciplina" SERIAL NOT NULL,
+    "nome_disciplina" VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE setor_siape (
-    id BIGINT NOT NULL,
-    PRIMARY KEY (id)
-);
+ALTER TABLE
+    "disciplina_ingresso" ADD PRIMARY KEY("id_disciplina");
+ALTER TABLE
+    "servidor" ADD CONSTRAINT "servidor_id_disciplina_foreign" FOREIGN KEY("id_disciplina") REFERENCES "disciplina_ingresso"("id_disciplina");
+ALTER TABLE
+    "servidor" ADD CONSTRAINT "servidor_id_telefones_foreign" FOREIGN KEY("id_telefones") REFERENCES "telefone_institucionais"("id_telefone");
+ALTER TABLE
+    "servidor" ADD CONSTRAINT "servidor_id_setor_foreign" FOREIGN KEY("id_setor") REFERENCES "setor_suap"("id_setor");
+ALTER TABLE
+    "servidor" ADD CONSTRAINT "servidor_id_campus_foreign" FOREIGN KEY("id_campus") REFERENCES "campus"("id_campus");
+ALTER TABLE
+    "servidor" ADD CONSTRAINT "servidor_id_jornada_foreign" FOREIGN KEY("id_jornada") REFERENCES "jornada_trabalho"("id_jornada");
+ALTER TABLE
+    "servidor" ADD CONSTRAINT "servidor_id_funcao_foreign" FOREIGN KEY("id_funcao") REFERENCES "funcao"("id_funcao");
